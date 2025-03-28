ccfea6b7a3f029918578c064280ee46f66e499bb
David Augusto <davidmaciel178@gmail.com>
2025-03-27 23:47:32 -0300

    atualizar
    atualizar

---
+++ ALGORITMOS-JS/Soma.js
@@ -0,0 +1,10 @@
+
+import readline from "readline-sync";
+
+let num1 = parseFloat(readline.question("Digite o primeiro número: "));
+let num2 = parseFloat(readline.question("Digite o segundo número: "));
+
+let soma = num1 + num2;
+
+console.log("A soma é: ", soma);
+
